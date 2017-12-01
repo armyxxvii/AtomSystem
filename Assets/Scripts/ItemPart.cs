a9def997a0dfd7a3fb4b25630641c924bae46e89
armyxxvii <armyxxvii@gmail.com>
2017-12-01 09:59:24 +0800

    更新資源

--- Assets/Scripts/ItemPart.cs
+++ Assets/Scripts/ItemPart.cs
@@ -1,20 +1,33 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
 
+[System.Serializable]
+public class PartNode
+{
+    public ItemPart part;
+    public GameObject part_Instance;
+    public GameObject app_Instance;
+    //    public int parentId;
+    public bool hideChild;
+    public List<int> childIds;
+}
+
+[RequireComponent (typeof(Collider))]
 public class ItemPart : MonoBehaviour
 {
+    public ItemTreeStructure item;
+
     [Header ("造型")]
     public GameObject appearance;
 
     [Header ("範圍")]
-    [Tooltip ("碰撞體")]
-    public Collider trigger;
+    [Tooltip("自動抓取\"第一個\"Trigger")]public Collider trigger;
 
     [Header ("硬度")]
-    [Tooltip ("硬度"),Range (0, 99)]
-    public int hardness;
+    [Range (0, 99)] public int hardness;
 
-    [Header ("原子")]
-    [Tooltip ("原子")]
+    [Header ("成分")]
     public Atom atom;
+    public List<Atom> atoms;
 
 }
--- Assets/Scripts/ItemTreeStructure.cs
+++ Assets/Scripts/ItemTreeStructure.cs
@@ -2,45 +2,53 @@
 using System.Collections.Generic;
 
 [System.Serializable]
-public struct PartNode
-{
-    public ItemPart part;
-    [System.NonSerialized] public GameObject partInstance;
-    public int parentId;
-    public bool hideChild;
-    public List<int> childIds;
-}
-
 public class ItemTreeStructure : MonoBehaviour
 {
+    [Header ("本體")]
     public PartNode rootNode;
+    [Header ("零件結構")]
     public List<PartNode> partNodes;
     [System.NonSerialized]public bool isGenerateComplate;
 
     void Start ()
     {
         foreach (PartNode pn in partNodes) {
-            if (pn.partInstance)
-                GameObject.Destroy (pn.partInstance);
+            if (pn.part_Instance)
+                GameObject.Destroy (pn.part_Instance);
         }
-        rootNode.partInstance = gameObject;
+        rootNode.part_Instance = gameObject;
+        rootNode.app_Instance = gameObject;
         isGenerateComplate = GeneratePart (rootNode, rootNode);
     }
 
     bool GeneratePart (PartNode _partNode, PartNode _rootNode)
     {
         if (_partNode.part) {
-            _partNode.partInstance = GameObject.Instantiate (_partNode.part.gameObject);
-            _partNode.partInstance.transform.parent = _rootNode.partInstance.transform;
-            _partNode.partInstance.SetActive (!_rootNode.hideChild);
             if (_partNode.part.appearance) {
-                GameObject _app = GameObject.Instantiate (_partNode.part.appearance);
-                _app.transform.parent = _partNode.partInstance.transform;
+                _partNode.app_Instance = Create (_partNode.part.appearance, _rootNode.app_Instance);
+                _partNode.app_Instance.SetActive (!_rootNode.hideChild);
             }
+            _partNode.part_Instance = Create (_partNode.part.gameObject, _partNode.app_Instance);
+            _partNode.part_Instance.SetActive (!_rootNode.hideChild);
         }
         foreach (int cId in _partNode.childIds) {
-            GeneratePart (partNodes [cId], _partNode);
+            PartNode c = partNodes [cId];
+            GeneratePart (c, _partNode);
         }
         return true;
+    }
+
+    GameObject Create (GameObject _source, GameObject _root = null)
+    {
+        GameObject _instance = GameObject.Instantiate (_source);
+        if (_root != null)
+            _instance.transform.parent = _root.transform;
+        _instance.transform.localPosition = Vector3.zero;
+        _instance.transform.localEulerAngles = Vector3.zero;
+        _instance.transform.localScale = Vector3.one;
+        ItemPart _part = _instance.GetComponent<ItemPart> ();
+        if (_part != null)
+            _part.item = this;
+        return _instance;
     }
 }
